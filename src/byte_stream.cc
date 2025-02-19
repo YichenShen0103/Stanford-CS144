f060e8ba60ffb3c91013182f083b8c070b09fe9e
ycshen vm <shenchiri576@gmail.com>
2025-02-20 00:28:10 +0800

    checkpoint 0 finished

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -1,56 +1,93 @@
 #include "byte_stream.hh"
+#include <stdexcept>
 
 using namespace std;
 
 ByteStream::ByteStream( uint64_t capacity ) : capacity_( capacity ) {}
 
-void Writer::push( string data )
+void Writer::push( string data ) noexcept
 {
-  (void)data; // Your code here.
+  auto len = min( data.size(), available_capacity() ); // 确定可写入的数据长度
+  if ( len == 0 ) {                                    // 如果可写入的数据长度为0，说明已经写满了，返回
+    return;
+  } else if ( len < data.size() ) { // 如果可写入的数据长度小于 data 的长度，说明只能写入部分数据
+    data.resize( len );             // 将 data 的长度截断为可写入的长度
+  }
+  // 将 data 写入到 buffer 中
+  buffer_data.push( move( data ) );
+  if ( buffer_data.size() == 1 ) // 写入前为空时需要更新 buffer_view
+    buffer_view = buffer_data.front();
+  // 更新已写入的数据长度
+  bytes_pushed_ += len;
 }
 
-void Writer::close()
+void Writer::close() noexcept
 {
-  // Your code here.
+  flag |= ( 1 << CLOSED );
 }
 
-bool Writer::is_closed() const
+void Writer::set_error() noexcept
 {
-  return {}; // Your code here.
+  flag |= ( 1 << ERROR );
 }
 
-uint64_t Writer::available_capacity() const
+bool Writer::is_closed() const noexcept
 {
-  return {}; // Your code here.
+  return flag & ( 1 << CLOSED );
 }
 
-uint64_t Writer::bytes_pushed() const
+uint64_t Writer::available_capacity() const noexcept
 {
-  return {}; // Your code here.
+  return capacity_ - reader().bytes_buffered();
 }
 
-string_view Reader::peek() const
+uint64_t Writer::bytes_pushed() const noexcept
 {
-  return {}; // Your code here.
+  return bytes_pushed_;
 }
 
-void Reader::pop( uint64_t len )
+string_view Reader::peek() const noexcept
 {
-  (void)len; // Your code here.
+  return buffer_view;
 }
 
-bool Reader::is_finished() const
+bool Reader::is_finished() const noexcept
 {
-  return {}; // Your code here.
+  return writer().is_closed() && ( bytes_buffered() == 0 );
 }
 
-uint64_t Reader::bytes_buffered() const
+bool Reader::has_error() const noexcept
 {
-  return {}; // Your code here.
+  return flag & ( 1 << ERROR );
 }
 
-uint64_t Reader::bytes_popped() const
+void Reader::pop( uint64_t len ) noexcept
 {
-  return {}; // Your code here.
+  if ( len > bytes_buffered() ) {
+    return;
+  }
+  // 更新已弹出的数据长度
+  bytes_popped_ += len;
+
+  // 将 buffer 中的数据弹出
+  while ( len > 0 ) {
+    if ( len >= buffer_view.size() ) {
+      len -= buffer_view.size();
+      buffer_data.pop();
+      buffer_view = buffer_data.front(); // 最开始就保证了 buffer_data 不为空
+    } else {
+      buffer_view.remove_prefix( len );
+      len = 0;
+    }
+  }
 }
 
+uint64_t Reader::bytes_buffered() const noexcept
+{
+  return writer().bytes_pushed() - bytes_popped();
+}
+
+uint64_t Reader::bytes_popped() const noexcept
+{
+  return bytes_popped_;
+}
--- src/byte_stream.hh
+++ src/byte_stream.hh
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <cstdint>
+#include <queue>
+#include <stdexcept>
 #include <string>
 #include <string_view>
 
@@ -9,48 +11,61 @@ class Writer;
 
 class ByteStream
 {
+protected:
+  enum State
+  {
+    CLOSED,
+    ERROR
+  };
+  uint64_t capacity_;
+  uint64_t bytes_pushed_ {};
+  uint64_t bytes_popped_ {};
+
+  unsigned char flag {}; // 0: normal, 1: closed, 2: error
+  std::queue<std::string> buffer_data {};
+  std::string_view buffer_view {};
+  bool error_ = {};
+
 public:
   explicit ByteStream( uint64_t capacity );
 
-  // Helper functions (provided) to access the ByteStream's Reader and Writer interfaces
   Reader& reader();
   const Reader& reader() const;
   Writer& writer();
   const Writer& writer() const;
 
-  void set_error() { error_ = true; };       // Signal that the stream suffered an error.
-  bool has_error() const { return error_; }; // Has the stream had an error?
-
-protected:
-  // Please add any additional state to the ByteStream here, and not to the Writer and Reader interfaces.
-  uint64_t capacity_;
-  bool error_ {};
+  void set_error() { error_ = true; }
+  bool has_error() const { return error_; }
 };
 
 class Writer : public ByteStream
 {
 public:
-  void push( std::string data ); // Push data to stream, but only as much as available capacity allows.
-  void close();                  // Signal that the stream has reached its ending. Nothing more will be written.
+  void push( std::string data ) noexcept; // 在可用容量允许的范围内向流中写入数据
 
-  bool is_closed() const;              // Has the stream been closed?
-  uint64_t available_capacity() const; // How many bytes can be pushed to the stream right now?
-  uint64_t bytes_pushed() const;       // Total number of bytes cumulatively pushed to the stream
+  void close() noexcept;     // 关闭流，不允许再向流中写入数据
+  void set_error() noexcept; // 流中出现错误，置位错误标志
+
+  bool is_closed() const noexcept;              // 判断流是否已关闭
+  uint64_t available_capacity() const noexcept; // 计算流中剩余可用容量
+  uint64_t bytes_pushed() const noexcept;       // 计算流中已写入的字节数
 };
 
 class Reader : public ByteStream
 {
 public:
-  std::string_view peek() const; // Peek at the next bytes in the buffer
-  void pop( uint64_t len );      // Remove `len` bytes from the buffer
+  std::string_view peek() const noexcept; // 返回流中下一个数据块的只读视图
+  void pop( uint64_t len ) noexcept;      // 从流中弹出指定长度的数据块
 
-  bool is_finished() const;        // Is the stream finished (closed and fully popped)?
-  uint64_t bytes_buffered() const; // Number of bytes currently buffered (pushed and not popped)
-  uint64_t bytes_popped() const;   // Total number of bytes cumulatively popped from stream
+  bool is_finished() const noexcept; // 判断流是否已关闭且所有数据块都已弹出
+  bool has_error() const noexcept;   // 判断流是否出现错误
+
+  uint64_t bytes_buffered() const noexcept; // 计算当前流中剩余的字节数
+  uint64_t bytes_popped() const noexcept;   // 计算流中已弹出的字节数
 };
 
 /*
- * read: A (provided) helper function thats peeks and pops up to `max_len` bytes
+ * read: A (provided) helper function thats peeks and pops up to `len` bytes
  * from a ByteStream Reader into a string;
  */
-void read( Reader& reader, uint64_t max_len, std::string& out );
+void read( Reader& reader, uint64_t len, std::string& out );
